0828d043e5d3c6c36d609f84c21d21992c899560
shima0 <shin.keeper@gmail.com>
2019-11-02 23:07:58 +0900

    update

--- src/train.py
+++ src/train.py
@@ -4,6 +4,11 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from tensorboardX import SummaryWriter
+# REF:
+# https://www.mlflow.org/docs/latest/python_api/mlflow.pytorch.html#module-mlflow.pytorch
+import mlflow.pytorch
+import mlflow
+
 from model.trans_NFCM import TransNFCM
 from optimizer.radam import RAdam
 from feature.data_loader_for_NFCM import FMNISTDataset, loader
@@ -26,28 +31,39 @@ idx2label = {0:"T-shirt/top",
 
 
 def main(args):
-    n_relational_embeddings = args.n_class**2
-    n_tag_embeddings = args.n_class
-    in_ch, out_ch = 1, 128
-    model = TransNFCM(in_ch, out_ch, n_tag_embeddings,
-                      embedding_dim=128).to(device)
+    with mlflow.start_run():
+        # Log our parameters into mlflow
+        for key, value in vars(args).items():
+            mlflow.log_param(key, value)
 
-    if Path(args.resume_model).exists():
-        print("load model:", args.resume_model)
-        model.load_state_dict(torch.load(args.resume_model))
+        n_relational_embeddings = args.n_class**2
+        n_tag_embeddings = args.n_class
+        in_ch, out_ch = 1, 128
+        model = TransNFCM(in_ch, out_ch, n_tag_embeddings,
+                        embedding_dim=128).to(device)
 
-    optimizer = RAdam(model.parameters(), weight_decay=1e-3)
+        if Path(args.resume_model).exists():
+            print("load model:", args.resume_model)
+            model.load_state_dict(torch.load(args.resume_model))
 
-    train_dataset = FMNISTDataset(n_class=args.n_class, train=True)
-    test_dataset = FMNISTDataset(n_class=args.n_class, train=False)
+        optimizer = RAdam(model.parameters(), weight_decay=1e-3)
 
-    train_loader = loader(train_dataset, args.batch_size)
-    test_loader = loader(test_dataset, 1, shuffle=False)
+        train_dataset = FMNISTDataset(n_class=args.n_class, train=True)
+        test_dataset = FMNISTDataset(n_class=args.n_class, train=False)
 
-    train(args, model, optimizer, train_loader)
-    test(args, model, test_loader,
-         show_image_on_board=args.show_image_on_board,
-         show_all_embedding=args.show_all_embedding)
+        train_loader = loader(train_dataset, args.batch_size)
+        test_loader = loader(test_dataset, 1, shuffle=False)
+
+        train(args, model, optimizer, train_loader)
+        test(args, model, test_loader,
+            show_image_on_board=args.show_image_on_board,
+            show_all_embedding=args.show_all_embedding)
+
+        # Upload the TensorBoard event logs as a run artifact
+        print("Uploading TensorBoard events as a run artifact...")
+        mlflow.log_artifacts(output_dir, artifact_path="events")
+        print("\nLaunch TensorBoard with:\n\ntensorboard --logdir=%s" %
+            os.path.join(mlflow.get_artifact_uri(), "events"))
 
 
 def train(args, model, optimizer, data_loader):
@@ -74,6 +90,9 @@ def train(args, model, optimizer, data_loader):
                        '{}/model.pth'.format(args.out_dir))
     torch.save(model.state_dict(),
                '{}/model.pth'.format(args.out_dir))
+
+    with mlflow.start_run():
+        mlflow.sklearn.log_model(model, '{}/model.pth'.format(args.out_dir))
 
 
 def test(args, model, data_loader,
@@ -106,6 +125,7 @@ def test(args, model, data_loader,
         writer.add_embedding(weights, label_img=images)
     else:
         writer.add_embedding(weights, metadata=labels)
+    # mlflow.log_metric('test_accuracy', test_accuracy, 0)
     print("done")
 
 
